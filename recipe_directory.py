4486faf1120f4232840646a2f1d87028ed225384
MacAttack824 <cmaclelland01@gmail.com>
2026-02-05 20:29:48 -0500

    added dictionary parts

--- recipe_directory.py
+++ recipe_directory.py
@@ -1,13 +1,23 @@
 # Here lies the recipe lists as a dictionary
 
-recipe_file{
-	{{name: tacos},{basic ingredients: [oil, salt, pepper, hot sauce, sour cream, taco seasoning]},
-	{ingredients: [a protein, tortillas, tomatoes, lettuce, corn]},
-	{cook time: 25 minutes},
-	{ID : 001},
-	{steps: JUST COOK IT},
-	{skill: easy}}
-
-	{name: pancakes}
-
+recipe_file = dict{
+	{{'name': 'tacos'}
+	{'basic_ingredients': ['oil', 'salt', 'pepper', 'hot sauce', 'sour cream', 'taco seasoning']},
+	{'ingredients': ['protein', 'tortillas', 'tomatoes', 'lettuce', 'corn']},
+	{'cook time': '25 minutes'},
+	{'ID' : 1},
+	{'steps' : 'JUST COOK IT'},
+	{'skill' : 'easy skill required'},
+    {'sides': ['rice', 'beans', 'guacamole', 'chips', 'salsa', ]}
 	}
+	{{'name': 'pancakes'},
+	{'basic_ingredients': ['oil, butter, milk, eggs']},
+    {'ingredents': ['pancake mix']},
+    {'cook time': '20 minutes'},
+    {'ID' : 2},
+    {'steps' : ['measure amount of pancake mix and mix well in a bowl', 'Add some oil to the pan and dollop a fist sized amount to start the pancake',
+              'once you see bubbles popping, it should be ready to flip to cook the other']}
+    {'skill' : 'medium skill required'}
+    {'sides': ['bacon', 'eggs', 'sausage', 'fruit', 'yogurt', 'biscuits']}
+	}
+}
