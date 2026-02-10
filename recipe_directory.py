9c8877ff0c1e9d6a300ee0ca2f1cdc8656fbb1e1
MacAttack824 <cmaclelland01@gmail.com>
2026-02-09 23:05:38 -0500

    tweaked more Monday night

--- recipe_directory.py
+++ recipe_directory.py
@@ -1,6 +1,9 @@
-# Here lies the recipe lists as a dictionary
+# Here lies the recipes as a class
 
-class Recipe: 
+class Recipe:
+
+	num_of_recipes = 0
+
 	def __init__(self, name: str, page: int, basic_ing: list[str], ingredients: list[str], cook_time: int, steps: list[str], suggested_sides:list[str], tags: list[str]):
 		self.name = name
 		self.page = page
@@ -10,16 +13,19 @@ class Recipe:
 		self.steps = steps
 		self.suggested_sides = suggested_sides
 		self.tags = tags
+		Recipe.num_of_recipes += 1
 
-def nametags(name, tags):
-    return name + ' = ' + ', '.join(tags)
+def nametags(self):
+    return '{} {}'.format(self.name, self.tags)
 
 recipe_1 = Recipe('Tacos', 1, ['oil', 'salt', 'pepper', 'hot sauce', 'sour cream', 'taco seasoning'], ['protein', 'tortillas', 'tomatoes', 'lettuce', 'corn'], 25, 'JUST COOK IT', ['rice', 'beans', 'guacamole', 'chips', 'salsa'], ['easy skill', 'mexican'])
 
-recipe_2 = Recipe('Pancakes', 2, ['oil, butter, milk, eggs'], ['pancake mix'], '20 minutes', ['measure amount of pancake mix and mix well in a bowl', 'Add some oil to the pan and dollop a fist sized amount to start the pancake',
+recipe_2 = Recipe('Pancakes', 2, ['oil, butter, milk, eggs'], ['pancake mix'], 20, ['measure amount of pancake mix and mix well in a bowl', 'Add some oil to the pan and dollop a fist sized amount to start the pancake',
 'once you see bubbles popping, it should be ready to flip to cook the other'], ['bacon', 'eggs', 'sausage', 'fruit', 'yogurt', 'biscuits'], 'medium skill')
 
-recipe_738 = Recipe('Cheeseburger Calzones', 738, ['flour', 'salt', 'pepper', 'oil', 'ketchup'], ['ground beef', 'cream cheese', 'shredded cheese', 'pickles', 'steak seasoning', 'pizza dough'], '60 minutes', 'steps', ['1000 island dressing', 'chips'], ['medium skill', 'tailgate party', 'italian'])
+recipe_738 = Recipe('Cheeseburger Calzones', 738, ['flour', 'salt', 'pepper', 'oil', 'ketchup'], ['ground beef', 'cream cheese', 'shredded cheese', 'pickles', 'steak seasoning', 'pizza dough'], 60, 'steps', ['1000 island dressing', 'chips'], ['medium skill', 'tailgate party', 'italian'])
+
+print(f'There are {Recipe.num_of_recipes} recipes in the notebook.')
 
 print(recipe_1.__dict__)
 
@@ -28,6 +34,5 @@ print(recipe_2.ingredients)
 print(recipe_1.name, f', ID page {recipe_1.page}')
 print(recipe_738.name)
 print(recipe_738.tags)
-print(recipe_738.name, recipe_738.tags)
-print(nametags(recipe_738.name, recipe_738.tags))
+print(nametags(recipe_738))
 
