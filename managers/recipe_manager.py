73c4d90145021166e067d9b09866f4192c778b83
MacAttack824 <cmaclelland01@gmail.com>
2026-02-25 18:40:35 -0500

    add self to methods

--- managers/recipe_manager.py
+++ managers/recipe_manager.py
@@ -17,11 +17,8 @@ class RecipeManager:
             print('now adding basic ingredients...')
             while True:
                 new_basic_ingredient = input('What basic kitchen ingredients are needed? Type done to finish: ')
-                if new_basic_ingredient == 'done':
+                if new_basic_ingredient == 'done' or new_basic_ingredient == '':
                     break
-                for i in range(2):
-                    if new_basic_ingredient == '':
-                        break
                 else:
                     basic_ingredients.append(new_basic_ingredient)
             filtered_basic_ingredients = [item for item in basic_ingredients if item not in ("")]
@@ -34,11 +31,8 @@ class RecipeManager:
             print('adding ingredients...')
             while True:
                 new_ingredient = input('What grocery ingredients do you need to purchase? Type done to finish: ')
-                if new_ingredient == 'done':
+                if new_ingredient == 'done' or new_ingredient == '':
                     break
-                for i in range(2):
-                    if new_ingredient == '':
-                        break
                 else:
                     ingredients.append(new_ingredient)
             filtered_ingredients = [item for item in ingredients if item not in ("")]
@@ -52,7 +46,7 @@ class RecipeManager:
                     cook_time = int(input('How long does this meal take to prepare? (in minutes): '))
                     break
                 except ValueError:
-                    print('please enter an integer in minutes')
+                    print('Please enter time in minutes')
             return (f'*** {cook_time}***')
 
         def add_steps():
@@ -60,11 +54,8 @@ class RecipeManager:
             print('Creating Steps...')
             while True:
                 new_step = input('Describe each step and hit enter. Type done to finish: ')
-                if new_step == 'done':
+                if new_step == 'done' or new_step == '':
                     break
-                for i in range(2):
-                    if new_step == '':
-                        break
                 else:
                     steps.append(new_step)
             filtered_steps = [item for item in steps if item not in ("")]
@@ -77,11 +68,8 @@ class RecipeManager:
         #     print('Creating a list of sides...')
         #     while True:
         #         new_side = input('Add sides to go with the main dish. Type done to finish: ')
-        #         if new_side == 'done':
+        #         if new_side == 'done' or new_side == '':
         #             break
-        #         for i in range(2):
-        #             if new_side == '':
-        #                 break
         #         else:
         #             sides.append(new_side)
         #     filtered_sides = [item for item in sides if item not in ("")]
@@ -94,11 +82,8 @@ class RecipeManager:
         #     print('Creating a list of tags...')
         #     while True:
         #         new_tag = input('Add tags that recipes can be filtered by. Type done to finish: ')
-        #         if new_tag == 'done':
+        #         if new_tag == 'done' or new_tag == '':
         #             break
-        #         for i in range(2):
-        #             if new_tag == '':
-        #                 break
         #         else:
         #             tags.append(new_tag)
         #     filtered_tags = [item for item in tags if item not in ('')]
